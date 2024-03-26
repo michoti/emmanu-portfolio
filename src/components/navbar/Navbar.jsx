518e00ee48a7982318a15c9fc2bc0cf91c8d7f72
shukeel <triggaillest@gmail.com>
2024-03-26 12:44:29 +0300

    modified nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -106,7 +106,7 @@ const Navbar = () => {
           </NavLink>
             
             <div className="lg:hidden">
-                <button className="navbar-burger flex items-center text-blue-600 p-3" onClick={handleMenuToggle}>
+                <button className="navbar-burger flex items-center text-[#C778DD] p-3" onClick={handleMenuToggle}>
                     <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                       <title>Mobile menu</title>
                       <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
@@ -139,7 +139,7 @@ const Navbar = () => {
                 <>
                     <div className="navbar-menu relative z-50">
                         <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25" onClick={handleMenuToggle}></div>
-                        <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+                        <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-[#282C33] border-r overflow-y-auto">
                             <div className="flex items-center mb-8">
                                 <NavLink to='/' className="mr-auto text-3xl font-bold leading-none">
                                       Emmanuel
