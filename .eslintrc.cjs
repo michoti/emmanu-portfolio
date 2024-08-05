5c3d370b9f8466ee10964150be7f672789614af7
shukeel <triggaillest@gmail.com>
2024-08-05 14:40:02 +0300

    Portfolio & about

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,12 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/prop-types': 0,
+    'no-unused-vars' : 0
   },
 }
